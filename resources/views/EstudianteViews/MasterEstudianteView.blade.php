b9bab6eff47b7240593f374721d94c3e8a425cef
Edward <edwardconw@gmail.com>
2019-08-14 22:33:41 -0500

    Pagina Principal Estudiante

--- resources/views/EstudianteViews/MasterEstudianteView.blade.php
+++ resources/views/EstudianteViews/MasterEstudianteView.blade.php
@@ -44,29 +44,6 @@ License: You must have a valid license purchased only from themeforest(the above
                 <!-- END RESPONSIVE MENU TOGGLER -->
                 <!-- BEGIN PAGE ACTIONS -->
                 <!-- DOC: Remove "hide" class to enable the page header actions -->
-                <div class="page-actions">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-circle btn-outline red dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-plus"></i>&nbsp;
-                            <span class="hidden-sm hidden-xs">Nuevo&nbsp;</span>&nbsp;
-                            <i class="fa fa-angle-down"></i>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="javascript:;">
-                                    <i class="icon-flag"></i> Plataforma
-                                    <span class="badge badge-success">OK</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:;">
-                                    <i class="icon-users"></i> Usuario
-                                    <span class="badge badge-danger">OK</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
                 <!-- END PAGE ACTIONS -->
                 <!-- BEGIN PAGE TOP -->
                 <div class="page-top">
@@ -237,7 +214,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <!-- BEGIN CONTAINER -->
         <div class="page-container">
             <!-- BEGIN SIDEBAR -->
-            @include('AdministradorViews.sidebarAdministrador')
+            @include('EstudianteViews.sidebarEstudiante')
 
             <!-- END SIDEBAR -->
             <!-- BEGIN CONTENT -->
@@ -246,88 +223,12 @@ License: You must have a valid license purchased only from themeforest(the above
                 <div class="page-content">
                     <!-- BEGIN PAGE HEADER-->
                     <!-- BEGIN THEME PANEL -->
-                    <div class="theme-panel">
-                        <div class="toggler tooltips" data-container="body" data-placement="left" data-html="true" data-original-title="Click to open advance theme customizer panel">
-                            <i class="icon-settings"></i>
-                        </div>
-                        <div class="toggler-close">
-                            <i class="icon-close"></i>
-                        </div>
-                        <div class="theme-options">
-                            <div class="theme-option theme-colors clearfix">
-                                <span> THEME COLOR </span>
-                                <ul>
-                                    <li class="color-default current tooltips" data-style="default" data-container="body" data-original-title="Default"> </li>
-                                    <li class="color-grey tooltips" data-style="grey" data-container="body" data-original-title="Grey"> </li>
-                                    <li class="color-blue tooltips" data-style="blue" data-container="body" data-original-title="Blue"> </li>
-                                    <li class="color-dark tooltips" data-style="dark" data-container="body" data-original-title="Dark"> </li>
-                                    <li class="color-light tooltips" data-style="light" data-container="body" data-original-title="Light"> </li>
-                                </ul>
-                            </div>
-                            <div class="theme-option">
-                                <span> Layout </span>
-                                <select class="layout-option form-control input-small">
-                                    <option value="fluid" selected="selected">Fluid</option>
-                                    <option value="boxed">Boxed</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Header </span>
-                                <select class="page-header-option form-control input-small">
-                                    <option value="fixed" selected="selected">Fixed</option>
-                                    <option value="default">Default</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Top Dropdown</span>
-                                <select class="page-header-top-dropdown-style-option form-control input-small">
-                                    <option value="light" selected="selected">Light</option>
-                                    <option value="dark">Dark</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Sidebar Mode</span>
-                                <select class="sidebar-option form-control input-small">
-                                    <option value="fixed">Fixed</option>
-                                    <option value="default" selected="selected">Default</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Sidebar Style</span>
-                                <select class="sidebar-style-option form-control input-small">
-                                    <option value="default" selected="selected">Default</option>
-                                    <option value="compact">Compact</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Sidebar Menu </span>
-                                <select class="sidebar-menu-option form-control input-small">
-                                    <option value="accordion" selected="selected">Accordion</option>
-                                    <option value="hover">Hover</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Sidebar Position </span>
-                                <select class="sidebar-pos-option form-control input-small">
-                                    <option value="left" selected="selected">Left</option>
-                                    <option value="right">Right</option>
-                                </select>
-                            </div>
-                            <div class="theme-option">
-                                <span> Footer </span>
-                                <select class="page-footer-option form-control input-small">
-                                    <option value="fixed">Fixed</option>
-                                    <option value="default" selected="selected">Default</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
                     <!-- END THEME PANEL -->
-                    <h1 class="page-title"> Bienvenido Administrador
+                    <h1 class="page-title"> Bienvenido Estudiante
                     </h1>
                     <!-- END PAGE HEADER-->
                     <div class="note note-info">
-                        <p> Pagina de Inicio del Adinistrador </p>
+                        <p> Pagina de Inicio del Estudiante </p>
                     </div>
                 </div>
                 <!-- END CONTENT BODY -->
---
+++ resources/views/EstudianteViews/sidebarEstudiante.blade.php
@@ -0,0 +1,218 @@
+        <!-- BEGIN SIDEBAR -->
+            <div class="page-sidebar-wrapper">
+                <!-- END SIDEBAR -->
+                <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+                <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+                <div class="page-sidebar navbar-collapse collapse">
+                    <!-- BEGIN SIDEBAR MENU -->
+                    <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
+                    <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
+                    <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+                    <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+                    <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
+                    <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+                    <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+                        <li class="nav-item start ">
+                            <a href="javascript:;" class="nav-link nav-toggle">
+                                <i class="icon-home"></i>
+                                <span class="title">Estudiante</span>
+                                <span class="arrow"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                <li class="nav-item start ">
+                                    <a href="dashboard_2.html" class="nav-link ">
+                                        <i class="icon-bulb"></i>
+                                        <span class="title">Gestion de Plataforma</span>
+                                        <span class="badge badge-success">OK</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                         <li class="nav-item  ">
+                            <a href="javascript:;" class="nav-link nav-toggle">
+                                <i class="icon-docs"></i>
+                                <span class="title">Plataforma</span>
+                                <span class="arrow"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                <li class="nav-item  ">
+                                    <a href="app_todo.html" class="nav-link ">
+                                        <i class="icon-clock"></i>
+                                        <span class="title">Todo 1</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="app_todo_2.html" class="nav-link ">
+                                        <i class="icon-check"></i>
+                                        <span class="title">Todo 2</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="app_inbox.html" class="nav-link ">
+                                        <i class="icon-envelope"></i>
+                                        <span class="title">Inbox</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="app_calendar.html" class="nav-link ">
+                                        <i class="icon-calendar"></i>
+                                        <span class="title">Calendar</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="app_ticket.html" class="nav-link ">
+                                        <i class="icon-notebook"></i>
+                                        <span class="title">Support</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                        <li class="nav-item  ">
+                            <a href="javascript:;" class="nav-link nav-toggle">
+                                <i class="icon-bar-chart"></i>
+                                <span class="title">Reportes</span>
+                                <span class="arrow"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                <li class="nav-item  ">
+                                    <a href="charts_amcharts.html" class="nav-link ">
+                                        <span class="title">amChart</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="charts_flotcharts.html" class="nav-link ">
+                                        <span class="title">Flot Charts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="charts_flowchart.html" class="nav-link ">
+                                        <span class="title">Flow Charts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="charts_google.html" class="nav-link ">
+                                        <span class="title">Google Charts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="charts_echarts.html" class="nav-link ">
+                                        <span class="title">eCharts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="charts_morris.html" class="nav-link ">
+                                        <span class="title">Morris Charts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="javascript:;" class="nav-link nav-toggle">
+                                        <span class="title">HighCharts</span>
+                                        <span class="arrow"></span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li class="nav-item ">
+                                            <a href="charts_highcharts.html" class="nav-link "> HighCharts </a>
+                                        </li>
+                                        <li class="nav-item ">
+                                            <a href="charts_highstock.html" class="nav-link "> HighStock </a>
+                                        </li>
+                                        <li class="nav-item ">
+                                            <a href="charts_highmaps.html" class="nav-link "> HighMaps </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                        <li class="nav-item  ">
+                            <a href="javascript:;" class="nav-link nav-toggle">
+                                <i class="icon-settings"></i>
+                                <span class="title">Configuracion</span>
+                                <span class="arrow"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                <li class="nav-item  ">
+                                    <a href="form_controls.html" class="nav-link ">
+                                        <span class="title">Bootstrap Form
+                                            <br>Controls</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_controls_md.html" class="nav-link ">
+                                        <span class="title">Material Design
+                                            <br>Form Controls</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_validation.html" class="nav-link ">
+                                        <span class="title">Form Validation</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_validation_states_md.html" class="nav-link ">
+                                        <span class="title">Material Design
+                                            <br>Form Validation States</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_validation_md.html" class="nav-link ">
+                                        <span class="title">Material Design
+                                            <br>Form Validation</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_layouts.html" class="nav-link ">
+                                        <span class="title">Form Layouts</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_repeater.html" class="nav-link ">
+                                        <span class="title">Form Repeater</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_input_mask.html" class="nav-link ">
+                                        <span class="title">Form Input Mask</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_editable.html" class="nav-link ">
+                                        <span class="title">Form X-editable</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_wizard.html" class="nav-link ">
+                                        <span class="title">Form Wizard</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_icheck.html" class="nav-link ">
+                                        <span class="title">iCheck Controls</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_image_crop.html" class="nav-link ">
+                                        <span class="title">Image Cropping</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_fileupload.html" class="nav-link ">
+                                        <span class="title">Multiple File Upload</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item  ">
+                                    <a href="form_dropzone.html" class="nav-link ">
+                                        <span class="title">Dropzone File Upload</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                      
+                        
+                    </ul>
+                    <!-- END SIDEBAR MENU -->
+                </div>
+                <!-- END SIDEBAR -->
+            </div>
